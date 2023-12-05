213d5610d99265fd5cd18a87314038f1344de6a0
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2023-12-05 18:24:33 +0530

    update

---
+++ config.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+
+if (fs.existsSync('config.env')) {
+  require('dotenv').config({
+      path: './config.env'
+  });
+}
+
+const toBool = (x) => x === 'true';
+
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
+
+module.exports = {
+  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
+  WARN_COUNT: process.env.WARN_COUNT || '3',
+  LOGS: toBool(process.env.LOGS) || true,
+  ANTILINK_ACTION: process.env.ANTI_LINK || 'kick',
+  ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
+  IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
+  SESSION_ID: process.env.SESSION_ID || 'ZnNneU5_J_A_R_V_I_S_ZcTU=',
+  LANG: process.env.LANG || 'EN',
+  HANDLERS: process.env.HANDLER  || 'null',
+  RMBG_KEY: process.env.RMBG_KEY || false,
+  BRANCH: 'main',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'ᴊᴀʀᴠɪꜱ;ᴡᴀʙᴏᴛ',
+  AUTO_STATUS_VIEWS: process.env.AUTO_STATUS_VIEWS || 'true',
+  WELCOME_MSG: process.env.WELCOME_MSG || 'Hi @user Welcome to @gname',
+  GOODBYE_MSG: process.env.GOODBYE_MSG || 'Hi @user It was Nice Seeing you',
+  AUTHOR: process.env.AUTHOR || 'ʟᴏᴋɪ-xᴇʀ',
+  MEDIA_DATA: process.env.MEDIA_DATA|| 'ʟɪꜱᴛ ᴍᴇɴᴜ;ᴊᴀʀᴠɪꜱ-ᴍᴅ;https://i.imgur.com/ltPPMTh.jpeg',
+  SUDO: process.env.SUDO || '917025673121,',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
+  BOT_INFO: process.env.BOT_INFO || 'ᴊᴀʀᴠɪꜱ;ʟᴏᴋɪ-xᴇʀ;https://i.imgur.com/VIpl3q5.mp4',
+  WORK_TYPE: process.env.WORK_TYPE || '',
+  DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
+};
