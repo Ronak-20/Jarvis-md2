eabf6f91fec3d86ed12eb20f84ae27528fbb45ee
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2023-12-12 13:35:06 +0530

    update
    no needed to updat this

--- plugins/anime.js
+++ plugins/anime.js
@@ -11,62 +11,43 @@ Jarvis - Loki-Xer
 
 
 const {
-	System,
-	isPrivate,
-	sendNeko,
-	tiny,
-	IronMan,
-	axios
+  tiny,
+  axios,
+  System,
+  IronMan,
+  isPrivate,
+  sendNeko,
 } = require("../lib/");
 
 
 System({
-		pattern: "waifu",
-		fromMe: isPrivate,
-		desc: "Send a waifu image",
-		type: "anime",
-	},
-	async (message, match) => {
-		try {
-			const api = await IronMan(`ironman/waifu`)
-			const response = await axios.get(api);
-			if (response.data.status) {
-				const imageUrl = response.data.ironman.url;
-				await message.send(imageUrl, {
-					caption: (tiny("*here is your waifu*")),
-					quoted: message
-				}, "image");
-			} else {
-				await message.send("Failed to fetch image");
-			}
-		} catch (error) {
-			console.error("Error fetching image:", error);
-			await message.send("Failed to fetch image.");
-		}
-	});
+	pattern: "waifu",
+	fromMe: isPrivate,
+	desc: "Send a waifu image",
+	type: "anime",
+},async (message, match) => {
+	try { const api = await IronMan(`ironman/waifu`)
+	const response = await axios.get(api);
+	if (response.data.status) {
+	const imageUrl = response.data.ironman.url;
+	await message.send(imageUrl, { caption: (tiny("*here is your waifu*")), quoted: message.data }, "image");
+	} else { await message.send("Failed to fetch image");
+	} } catch (error) { console.error("Error fetching image:", error);
+	await message.send("Failed to fetch image.");}
+});
 
 
 System({
-		pattern: "neko",
-		fromMe: isPrivate,
-		desc: "Send Neko images",
-		type: "anime",
-	},
-	async (message, match) => {
-		try {
-			const api = await IronMan(`ironman/neko`)
-			const response = await axios.get(api);
-			if (response.data.status) {
-				const imageUrl = response.data.ironman.url;
-				await message.send(imageUrl, {
-					caption: (tiny("*here is your neko*")),
-					quoted: message
-				}, "image");
-			} else {
-				await message.send("Failed to fetch image");
-			}
-		} catch (error) {
-			console.error("Error fetching image:", error);
-			await message.send("Failed to fetch image.");
-		}
-	});
+	pattern: "neko",
+	fromMe: isPrivate,
+	desc: "Send Neko images",
+	type: "anime",
+}, async (message, match) => {
+	try { const api = await IronMan(`ironman/neko`)
+	const response = await axios.get(api);
+	if (response.data.status) { const imageUrl = response.data.ironman.url;
+	await message.send(imageUrl, { caption: (tiny("*here is your neko*")), quoted: message.data }, "image");
+	} else { await message.send("Failed to fetch image");
+	} } catch (error) { console.error("Error fetching image:", error);
+	await message.send("Failed to fetch image.");}
+});
