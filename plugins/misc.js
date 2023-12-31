0c6dc077888096c6c74558446488f3762ece4f65
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2023-12-31 13:36:57 +0530

    Fixed

--- plugins/misc.js
+++ plugins/misc.js
@@ -18,6 +18,7 @@ const {
 } = require("../lib/");
 const config = require('../config');
 
+
 System({
         pattern: "wm",
 	fromMe: isPrivate,
@@ -28,49 +29,73 @@ System({
 	await message.reply(sender);
 });
 
+
 System({
-        pattern: "save", 
-        fromMe: true,
-        desc: "used to save messages", 
-        type: "misc",
-    },
+    pattern: "save", 
+    fromMe: true,
+    desc: "used to save messages", 
+    type: "misc",
+},
 async (message, match) => {
-    return await message.forward(message.dm, message.reply_message, { quoted: message.data });
+    if (!message.reply_message) {
+        return;
+    } else {
+        await message.forward(message.dm, message.reply_message, { quoted: message.data });
+    }
 });
 
-System({
-	pattern: "attp",
-	fromMe: isPrivate,
-	desc: "Text to animated sticker",
-	type: "converter",
-}, async (message, match) => {
-	try { match = match || (message.reply_message && message.reply_message.text);
-	if (!match) { return await message.reply("_Give me some text_");
-	} else { await message.send("_making text into attp it take upto 1 minute_");
-	const buff = await LokiXer(`attp?text=${encodeURIComponent(match)}`);
-	const stickerPackNameParts = config.STICKER_PACKNAME.split(";");
-	const packname = stickerPackNameParts[0];
-	const author = stickerPackNameParts[1];
-	await message.send(buff, { packname, author }, "sticker"); }
-	} catch (error) { console.error("An error occurred:", error);
-	await message.reply("An error occurred while fetching the API data.");}
-});
 
 System({
-	pattern: "ss",
-	fromMe: isPrivate,
-	desc: "taking website screenshot",
-	type: "tool",
+    pattern: "attp",
+    fromMe: isPrivate,
+    desc: "Text to animated sticker",
+    type: "converter",
 }, async (message, match) => {
-	try { match = match || (message.reply_message && message.reply_message.text);
-	if (!match) { return await message.reply("_Give me a web URL eg: https://lokiser.xyz/_");
-	} else if (match.startsWith("https")) {
-	const ss = await LokiXer(`ssweb?link=${match}`);
-	return await message.client.sendMessage(message.chat, { image: { url: ss } });
-	} else { return await message.reply("_Give me a web URL starting with 'https' eg: https://lokiser.xyz/_"); }
-	} catch (error) { console.error("An error occurred:", error);
-	await message.reply("An error occurred while fetching the API data.");}
+    try {
+        match = match || (message.reply_message && message.reply_message.text);
+
+        if (!match) {
+            return await message.reply("_Give me some text_");
+        } else {
+            await message.send("_making text into attp, it may take up to 1 minute_");
+            const buff = await LokiXer(`attp?text=${encodeURIComponent(match)}`);
+            const stickerPackNameParts = config.STICKER_PACKNAME.split(";");
+            const packname = stickerPackNameParts[0];
+            const author = stickerPackNameParts[1];
+            await message.send(buff, { packname, author }, "sticker");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        await message.reply("An error occurred while fetching the API data.");
+    }
 });
+
+
+System({
+    pattern: "attp",
+    fromMe: isPrivate,
+    desc: "Text to animated sticker",
+    type: "converter",
+}, async (message, match) => {
+    try {
+        match = match || (message.reply_message && message.reply_message.text);
+
+        if (!match) {
+            return await message.reply("_Give me some text_");
+        } else {
+            await message.send("_making text into attp, it may take up to 1 minute_");
+            const buff = await LokiXer(`attp?text=${encodeURIComponent(match)}`);
+            const stickerPackNameParts = config.STICKER_PACKNAME.split(";");
+            const packname = stickerPackNameParts[0];
+            const author = stickerPackNameParts[1];
+            await message.send(buff, { packname, author }, "sticker");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        await message.reply("An error occurred while fetching the API data.");
+    }
+});
+
 
 System({
     pattern: "bitly",
@@ -79,53 +104,66 @@ System({
     type: "misc",
 }, async (message, match) => {
     try {
-    match = match || message.reply_message.text;
-    if (!match) return await message.reply("_Reply to a URL or enter a URL_");
-    if (!isUrl(match)) return await message.reply("_Not a valid URL_");
-    let short = await Bitly(match);
-    return await message.reply(short.link);
+        match = match || (message.reply_message && message.reply_message.text);
+
+        if (!match) {
+            return await message.reply("_Reply to a URL or enter a URL_");
+        }
+
+        if (!isUrl(match)) {
+            return await message.reply("_Not a valid URL_");
+        }
+
+        let short = await Bitly(match);
+        return await message.reply(short.link);
     } catch (error) {
-    console.error("An error occurred:", error);
-    return await message.reply("_An error occurred while shortening the URL._");
+        console.error("An error occurred:", error);
+        return await message.reply("_An error occurred while shortening the URL._");
     }
 });
 
+
 System({
-	pattern: 'whois ?(.*)',
-	type: 'user',
-	desc: 'to find how is'
+    pattern: 'whois ?(.*)',
+    type: 'user',
+    desc: 'to find how is'
 }, async (message, match) => {
-	try {
-	let user = message.reply_message.sender || match.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-	let pp;
-	if (!user) return message.send('_Need a User!');
-	try {
-	pp = await message.client.profilePictureUrl(user, 'image');
-	} catch {
-	pp = 'https://i.imgur.com/b3hlzl5.jpg';
-	}
-	let status = await message.client.fetchStatus(user);
-	const date = new Date(status.setAt);
-	const options = {
-	year: 'numeric',
-	month: 'long',
-	day: 'numeric',
-	hour: 'numeric',
-	minute: 'numeric',
-	second: 'numeric'
-	};
-	let wm = 'https://wa.me/' + (message.reply_message.sender || message.mention[0] || message.text).split('@')[0];
-	let nu = '' + (message.reply_message.sender || message.mention[0] || message.text).split('@')[0];
-	const setAt = date.toLocaleString('en-US', options);
-	await message.send({
-	url: pp
-	}, {
-	caption: `*Name :* @${user.replace(/[^0-9]/, '')}\n*About :* ${status.status}\n*About Set Date :* ${setAt}\n*whatsapp :* ${wm}`,
-	quoted: message
-	},
-	'image'
-	);
-	} catch (e) {
-		return await message.send('Failed');
-	}
+    try {
+        let user = message.reply_message?.sender || (match ? match.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : null);
+        if (!user) {
+            return message.send('_Need a User!');
+        }
+
+        let pp;
+        try {
+            pp = await message.client.profilePictureUrl(user, 'image');
+        } catch {
+            pp = 'https://i.imgur.com/b3hlzl5.jpg';
+        }
+
+        let status = await message.client.fetchStatus(user);
+        const date = new Date(status.setAt);
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        };
+
+        let wm = 'https://wa.me/' + user.split('@')[0];
+        const setAt = date.toLocaleString('en-US', options);
+
+        await message.send({
+            url: pp
+        }, {
+            caption: `*Name :* @${user.replace(/[^0-9]/g, '')}\n*About :* ${status.status}\n*About Set Date :* ${setAt}\n*whatsapp :* ${wm}`,
+            quoted: message
+        },
+        'image');
+    } catch (e) {
+        console.error('An error occurred:', e);
+        return await message.send('Failed');
+    }
 });
