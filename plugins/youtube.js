60cff141f33ba9e06a84ca06a235cadd5cbb3bd5
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-01-07 15:19:17 +0530

    find added

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,18 +1,25 @@
 const {
   yts,
   tiny,
+  sign,
   isUrl,
   System,
   GetYta,
   GetYtv,
   toAudio,
+  audioCut,
   Ytsearch,
   getBuffer,
   isPrivate,
   AddMp3Meta,
+  buildStringToSign,
   extractUrlFromMessage,
 } = require('../lib/');
 const config = require('../config');
+const FormData = require('form-data')
+const fetch = require('node-fetch')
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs')
 
 System(
   {
@@ -276,3 +283,72 @@ System({
     console.error('An error occurred:', error);
   }
 });
+
+System(
+  {
+    pattern: 'find',
+    fromMe: true,
+    desc: 'Find the Song',
+    type: 'misc',
+  },
+  async (message, match, m) => {
+    if (!message.reply_message || (!message.reply_message.audio && !message.reply_message.video)) {
+      return await message.reply('*Reply to audio or video*');
+    }
+
+    const p = await message.quoted.downloadAndSave();
+    const data = await audioCut(p, 0, 15);
+    const current_data = new Date();
+    const timestamp = current_data.getTime() / 1000;
+
+    const options = {
+      host: 'identify-eu-west-1.acrcloud.com',
+      endpoint: '/v1/identify',
+      signature_version: '1',
+      data_type: 'audio',
+      secure: true,
+      access_key: '8c21a32a02bf79a4a26cb0fa5c941e95',
+      access_secret: 'NRSxpk6fKwEiVdNhyx5lR0DP8LzeflYpClNg1gze',
+    };
+
+    const stringToSign = buildStringToSign(
+      'POST',
+      options.endpoint,
+      options.access_key,
+      options.data_type,
+      options.signature_version,
+      timestamp
+    );
+
+    const signature = sign(stringToSign, options.access_secret);
+
+    const form = new FormData();
+    form.append('sample', data);
+    form.append('sample_bytes', data.length);
+    form.append('access_key', options.access_key);
+    form.append('data_type', options.data_type);
+    form.append('signature_version', options.signature_version);
+    form.append('signature', signature);
+    form.append('timestamp', timestamp);
+
+    const res = await fetch('http://' + options.host + options.endpoint, {
+      method: 'POST',
+      body: form,
+    });
+
+    const { status, metadata } = await res.json();
+    if (status.msg !== 'Success') {
+      return await message.reply(status.msg);
+    }
+
+    const { album, release_date, artists, title } = metadata.music[0];
+    const videos = await yts(title);
+
+    await message.client.sendMessage(m.jid, {
+      image: { url: `${videos.all[0].image}` },
+      caption: `*Title :* ${title}\n*Album :* ${album.name || ''}\n*Artists :* ${
+        artists !== undefined ? artists.map((v) => v.name).join(', ') : ''
+      }\n*Release Date :* ${release_date}`,
+    });
+  }
+);
