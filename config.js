d9125f65d0107bc3b305d64c6fccd9dad1e0b833
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-01-29 15:06:28 +0530

    update

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ const toBool = (x) => x === 'true';
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
 
 module.exports = {
-  ANTILINK: toBool(process.env.ANTI_LINK) || false,
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   LOGS: toBool(process.env.LOGS) || true,
-  ANTILINK_ACTION: process.env.ANTI_LINK || 'kick',
+  KOYEB_API: process.env.KOYEB_API || '',
+  SERVER: process.env.SERVER || 'defeat',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
   SESSION_ID: process.env.SESSION_ID || 'ZnNneU5_J_A_R_V_I_S_ZcTU=',
