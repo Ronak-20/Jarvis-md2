ca5273a339ab9d9433a2c3147c0a218de08f8d09
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-01-30 18:56:38 +0530

    update

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ module.exports = {
   WARN_COUNT: process.env.WARN_COUNT || '3',
   LOGS: toBool(process.env.LOGS) || true,
   KOYEB_API: process.env.KOYEB_API || '',
+  SERVERURL: process.env.SERVERURL || '',
   SERVER: process.env.SERVER || 'defeat',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
