f2adce751e7cd3523c10333a28196875a641f376
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-01-31 00:57:16 +0530

    fixed

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,24 +1,22 @@
-const {
-    System,
-    getAlive,
-    setAlive,
-    alive,
-    isPrivate
-} = require('../lib')
+const { System, getAlive, setAlive, alive, isPrivate } = require('../lib');
 
 System({
     pattern: 'alive ?(.*)',
     fromMe: isPrivate,
-    desc: 'Does bot work?',
+    desc: 'Check if the bot is alive',
     type: 'user'
 },
 async (message, match) => {
-    const msg = await getAlive();
-    if(match == "get" && message.sudo.includes(message.sender)){
-    return await message.send(msg);
-    } else if(match && message.sudo.includes(message.sender)) {
-    await setAlive(match);
-    return await message.send('_Alive Updated_');
+    try {
+        const msg = await getAlive();
+        if (match && match[1] === "get" && message.sudo.includes(message.sender)) {
+            return await message.send(msg);
+        } else if (match && message.sudo.includes(message.sender)) {
+            await setAlive(match[1]);
+            return await message.send('_Alive Updated_');
+        }
+        return await alive(message, msg);
+    } catch (error) {
+        console.error('Error in alive command:', error);
     }
-    return await alive(message,msg);
 });
